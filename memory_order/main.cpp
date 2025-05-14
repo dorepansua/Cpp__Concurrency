c5c32793bef52d1cfccfbbe2af170dae3634ea98
Hung Vu <dorepansua@gmail.com>
2025-05-14 14:02:09 +0700

    Spin lock

--- memory_order/main.cpp
+++ memory_order/main.cpp
@@ -1,4 +1,58 @@
+
+#include <iostream>
+#include <atomic>
+#include <stdlib.h>
+#include <thread>
+
+class SpinLock
+{
+    std::atomic_flag m_flag = ATOMIC_FLAG_INIT; 
+
+public:
+    SpinLock() {}
+
+
+    void lock()
+    {
+        while (!m_flag.test_and_set(std::memory_order_acquire)) {
+            std::cout<<"Pending..."<<std::endl;
+        }
+    }
+
+    void unlock()
+    {
+        m_flag.clear(std::memory_order_release);
+    }
+};
+
+
+SpinLock spinLock;
+
+
+int global_count = 0;
+
+void DoSomeThing()
+{
+    spinLock.lock();
+    for (int i = 0; i < 100000 ; ++i) {
+        global_count++;
+    }
+    spinLock.unlock();
+}
+
 int main()
 {
-    return 1;
+    std::thread t1(DoSomeThing);
+    std::thread t2(DoSomeThing);
+
+
+
+    t1.join();
+    t2.join();
+
+
+    std::cout<<"global_count = "<<global_count;
+
+    system("pause");
+    return 0;
 }
